bf5179dcf7d5d5f223680d684ad670fd5964477b
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2020-11-23 22:55:08 +0900

    [CREATE] Project Entity

---
+++ src/main/java/com/dsm/moi/domains/domain/Project.java
@@ -0,0 +1,101 @@
+package com.dsm.moi.domains.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
+public class Project {
+
+    @Id
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "closing_date")
+    private LocalDate closingDate;
+
+    @Column(name = "writer")
+    private String writer;
+
+    @Column(name = "personnel")
+    private String personnel;
+
+    @Column(name = "hashtag")
+    private String hashtag;
+
+    @Column(name = "profile")
+    private String profile;
+
+    public Project() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDate getClosingDate() {
+        return closingDate;
+    }
+
+    public void setClosingDate(LocalDate closingDate) {
+        this.closingDate = closingDate;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    public String getPersonnel() {
+        return personnel;
+    }
+
+    public void setPersonnel(String personnel) {
+        this.personnel = personnel;
+    }
+
+    public String getHashtag() {
+        return hashtag;
+    }
+
+    public void setHashtag(String hashtag) {
+        this.hashtag = hashtag;
+    }
+
+    public String getProfile() {
+        return profile;
+    }
+
+    public void setProfile(String profile) {
+        this.profile = profile;
+    }
+}
