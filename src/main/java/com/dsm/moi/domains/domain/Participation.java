3c520745db8e19682292901f9bb872f091f2a9bc
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2020-11-24 13:15:08 +0900

    [UPDATE] <Participation> Columns

--- src/main/java/com/dsm/moi/domains/domain/Participation.java
+++ src/main/java/com/dsm/moi/domains/domain/Participation.java
@@ -1,10 +1,76 @@
 package com.dsm.moi.domains.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "participation")
 public class Participation {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id", referencedColumnName = "id")
+    private Student student;
+
+    @ManyToOne
+    @JoinColumn(name = "project_id", referencedColumnName = "id")
+    private Project project;
+
+    @Column(name = "student_area")
+    private String area;
+
+    @Column(name = "is_passed")
+    private boolean isPassed;
+
+    public Participation() {}
+    public Participation(int id, Student student, Project project, String area, boolean isPassed) {
+        this.id = id;
+        this.student = student;
+        this.project = project;
+        this.area = area;
+        this.isPassed = isPassed;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Project getProject() {
+        return project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public boolean isPassed() {
+        return isPassed;
+    }
+
+    public void setPassed(boolean passed) {
+        isPassed = passed;
+    }
 }
