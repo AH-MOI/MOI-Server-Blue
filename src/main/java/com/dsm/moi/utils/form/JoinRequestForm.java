23edc74d281505cfa6eda35eb0b8e0f0e38f7dd0
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2020-11-24 09:14:13 +0900

    [CREATE] <JoinRequestForm> JoinRequestForm

---
+++ src/main/java/com/dsm/moi/utils/form/JoinRequestForm.java
@@ -0,0 +1,63 @@
+package com.dsm.moi.utils.form;
+
+import org.hibernate.mapping.Join;
+
+import java.time.LocalDate;
+
+public class JoinRequestForm {
+
+    private String id;
+    private String password;
+    private String name;
+    private LocalDate birthday;
+    private String school;
+
+    public JoinRequestForm() {}
+    public JoinRequestForm(String id, String password, String name, LocalDate birthday, String school) {
+        this.id = id;
+        this.password = password;
+        this.name = name;
+        this.birthday = birthday;
+        this.school = school;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+}
