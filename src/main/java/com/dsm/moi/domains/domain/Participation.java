0cd1d0f5e470634b04382c800ea381df00d243b8
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2020-11-24 13:05:41 +0900

    [CREATE] <Participation> Participation

---
+++ src/main/java/com/dsm/moi/domains/domain/Participation.java
@@ -0,0 +1,10 @@
+package com.dsm.moi.domains.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "participation")
+public class Participation {
+
+}
