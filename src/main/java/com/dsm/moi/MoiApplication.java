bff4fab3a2c6b26468998a3d2d760fb276348f8c
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2020-11-24 09:11:29 +0900

    [UPDATE] exception and test

--- src/main/java/com/dsm/moi/MoiApplication.java
+++ src/main/java/com/dsm/moi/MoiApplication.java
@@ -3,11 +3,11 @@ package com.dsm.moi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDate;
+
 @SpringBootApplication
 public class MoiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MoiApplication.class, args);
 	}
-
 }
--- src/main/java/com/dsm/moi/utils/exception/RuleViolationInformationException.java
+++ src/main/java/com/dsm/moi/utils/exception/RuleViolationInformationException.java
@@ -1,2 +1,7 @@
-package com.dsm.moi.utils.exception;public class RuleViolationInformationException {
+package com.dsm.moi.utils.exception;
+
+public class RuleViolationInformationException extends RuntimeException {
+    public RuleViolationInformationException() {
+        super("회원의 정보가 잘못되었습니다.");
+    }
 }
