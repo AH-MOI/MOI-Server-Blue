801fa96a2276f6bd3dae7793a60297406abfaa08
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2020-11-24 09:22:26 +0900

    [UPDATE] <AuthController> join

--- src/main/java/com/dsm/moi/controller/AuthController.java
+++ src/main/java/com/dsm/moi/controller/AuthController.java
@@ -1,14 +1,34 @@
 package com.dsm.moi.controller;
 
+import com.dsm.moi.domains.domain.Student;
+import com.dsm.moi.domains.service.AuthService;
+import com.dsm.moi.utils.form.JoinRequestForm;
+import org.apache.catalina.valves.StuckThreadDetectionValve;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/auth")
 public class AuthController {
 
-    @PostMapping(value = "/join")
-    public void join() {
+    private AuthService authService;
 
+    @Autowired
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping(value = "/join")
+    public void join(@RequestBody JoinRequestForm form) {
+        Student student = new Student();
+
+        student.setId(form.getId());
+        student.setPassword(form.getPassword());
+        student.setName(form.getName());
+        student.setBirthday(form.getBirthday());
+        student.setSchool(form.getSchool());
+
+        authService.join(student);
     }
 
     @PostMapping(value = "/login")
