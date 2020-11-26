a1c0464cfea6af74a330debcd53b324032d457c2
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2020-11-26 22:13:20 +0900

    [FINALLY]

--- src/main/java/com/dsm/moi/configuration/DatabaseConfiguration.java
+++ src/main/java/com/dsm/moi/configuration/DatabaseConfiguration.java
@@ -10,26 +10,19 @@ import javax.sql.DataSource;
 @Configuration
 public class DatabaseConfiguration {
 
-    @Value("com.mysql.cj.jdbc.Driver")
-    private String driverClassName;
-
-    @Value("jdbc:mysql://moi-rds.cfc2yl6t4e27.ap-northeast-2.rds.amazonaws.com/moi?useUnicode=true&characterEncoding=utf8&allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC")
-    private String url;
-
-    @Value("admin")
-    private String username;
-
-    @Value("asdfjkl;")
-    private String password;
+    private final static String DRIVER_CLASS_NAME = "com.mysql.cj.jdbc.Driver";
+    private final static String URL = "jdbc:mysql://moi-rds.cfc2yl6t4e27.ap-northeast-2.rds.amazonaws.com/moi?useUnicode=true&characterEncoding=utf8&allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC";
+    private final static String USERNAME = "admin";
+    private final static String PASSWORD = "asdfjkl;";
 
     @Bean
     public DataSource dataSource() {
         return DataSourceBuilder
                 .create()
-                .driverClassName(driverClassName)
-                .url(url)
-                .username(username)
-                .password(password)
+                .driverClassName(DRIVER_CLASS_NAME)
+                .url(URL)
+                .username(USERNAME)
+                .password(PASSWORD)
                 .build();
     }
 }
